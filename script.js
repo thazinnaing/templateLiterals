f962ef5c45f4585fa155391266b70e07ef3b3d60
thazin naing <thazinaingmm@gmail.com>
2023-04-27 21:03:10 +0630

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const bodyTag=document.getElementsByTagName("body")[0]
+const country="Myanmar"
+
+bodyTag.innerHTML=`
+<div class="container">
+        <div class="wrapper">
+            <h1>Welcome to ${country}</h1>
+        </div>
+        <div class="welcometext">
+            <div class="text">welcome to my heart</div>
+        </div>
+    </div>
+`
---
+++ stringliterals.js
@@ -0,0 +1,21 @@
+const stringtest=
+"My name is Thazin Naing."+
+"I am 22 years old."+
+"I like vegetablrs."+
+"I love PuTuu."
+
+const anotherstring="awesome"
+
+concatinatestring=
+"My name is Thazin Naing."+
+"I am 22 years old."+
+"I like vegetablrs."+
+"I love "+anotherstring+" PuTuu."
+
+//template literals
+
+const templateString=`My name is Thazin "Naing" I am "'1' year old" I am ${anotherstring}`
+
+const num1=10;
+const num2=20;
+const twoNumbers=`Adding two numbers is :${num1+num2}`
